7bc3e215ee61c6952f8adddb37ee760c3f5687ed
Daniel Tralamazza <tralamazza@gmail.com>
2013-10-28 20:59:50 +0100

    usb: pass non device requets to function handlers

--- examples/hid/hid_impl.c
+++ examples/hid/hid_impl.c
@@ -3,36 +3,51 @@
 #include "hid_impl.h"
 
 enum hid_ctrl_req_code {
-		USB_CTRL_REQ_HID_GET_REPORT = 0x01,
-		USB_CTRL_REQ_HID_GET_IDLE = 0x02,
-		USB_CTRL_REQ_HID_GET_PROTOCOL = 0x03,
-		USB_CTRL_REQ_HID_SET_REPORT = 0x09,
-		USB_CTRL_REQ_HID_SET_IDLE = 0x0A,
-		USB_CTRL_REQ_HID_SET_PROTOCOL = 0x0B
+	USB_CTRL_REQ_HID_GET_REPORT = 0x01,
+	USB_CTRL_REQ_HID_GET_IDLE = 0x02,
+	USB_CTRL_REQ_HID_GET_PROTOCOL = 0x03,
+	USB_CTRL_REQ_HID_SET_REPORT = 0x09,
+	USB_CTRL_REQ_HID_SET_IDLE = 0x0A,
+	USB_CTRL_REQ_HID_SET_PROTOCOL = 0x0B
 };
+
+/*enum hid_report_type {
+	USB_HID_REPORT_TYPE_INPUT = 0x01,
+	USB_HID_REPORT_TYPE_OUTPUT = 0x02,
+	USB_HID_REPORT_TYPE_FEATURE = 0x03
+};*/
+
+static int
+hid_handle_control_class(struct usb_ctrl_req_t *req, struct hid_ctx *ctx)
+{
+	switch ((enum hid_ctrl_req_code)req->bRequest) {
+	// case USB_CTRL_REQ_HID_GET_REPORT:
+	// case USB_CTRL_REQ_HID_GET_IDLE:
+	// case USB_CTRL_REQ_HID_GET_PROTOCOL:
+	// case USB_CTRL_REQ_HID_SET_REPORT:
+	// case USB_CTRL_REQ_HID_SET_IDLE:
+	// case USB_CTRL_REQ_HID_SET_PROTOCOL:
+	default:
+		return (0);
+	}
+}
 
 static int
 hid_handle_control(struct usb_ctrl_req_t *req, void *data)
 {
-	printf("!!!!!!!!! ReqType %x | Req %x !!!!!!!!!\n", req->bmRequestType, req->bRequest);
-	printf("!!!!!!!!! recp %x | type %x | in %x !!!!!!!!!\n", req->recp, req->type, req->in);
 	struct hid_ctx *ctx = data;
-	switch ((enum hid_ctrl_req_code)req->bRequest) {
-	case USB_CTRL_REQ_HID_GET_REPORT:
-		usb_ep0_tx_cp(&ctx->report, ctx->report_size, req->wLength, NULL, NULL);
+
+	if (req->type == USB_CTRL_REQ_CLASS)
+		return (hid_handle_control_class(req, ctx));
+
+	switch (req->bRequest) {
+	case USB_CTRL_REQ_GET_DESCRIPTOR:
+		usb_ep0_tx_cp(ctx->report, ctx->report_size, req->wLength, NULL, NULL);
 		usb_handle_control_status(0);
-		break;
-		// USB_CTRL_REQ_HID_GET_IDLE
-		// USB_CTRL_REQ_HID_GET_PROTOCOL
-		// USB_CTRL_REQ_HID_SET_REPORT
-	case USB_CTRL_REQ_HID_SET_IDLE:
-		// printf("SET IDLE: REPORT ID %d, DURATION %d\n", req->wValueLow, req->wValueHigh);
 		return (0);
-		// USB_CTRL_REQ_HID_SET_PROTOCOL
 	default:
-		return (0);
+		return (1);
 	}
-	return (1);
 }
 
 const struct usbd_function hid_function = {
