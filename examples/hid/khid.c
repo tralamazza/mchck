40b0551c96e12138c4a51d2b1394a0832a63f516
Daniel Tralamazza <tralamazza@gmail.com>
2013-10-30 13:04:43 +0100

    just send mouse_data on every int in cb

--- examples/hid/khid.c
+++ examples/hid/khid.c
@@ -7,11 +7,49 @@ static void init_my_hid(int config);
 #include "desc.h"
 
 static struct hid_ctx hid_ctx;
+static uint8_t idle_duration = 0;
+
+static size_t
+hid_get_descriptor(enum hid_report_descriptor_type type, uint8_t index, void **data_out)
+{
+	if (type != USB_HID_REPORT_DESC_TYPE_REPORT)
+		return (0);
+	*data_out = report_desc; // see desc.h
+	return (REPORT_DESC_SIZE);
+}
+
+static size_t
+hid_get_report(enum hid_report_type type, uint8_t report_id, void **data_out)
+{
+	if (type != USB_HID_REPORT_TYPE_INPUT)
+		return (0);
+	*data_out = &mouse_data;
+	return (3);
+}
 
 static void
-init_my_hid(int config)
+hid_set_idle(const uint8_t duration, const uint8_t report_id)
 {
-	hid_init(&hid_descriptor, &hid_ctx);
+	idle_duration = duration;
+}
+
+static struct hid_user_functions_t hid_funcs = {
+	.get_descriptor = hid_get_descriptor,
+	.get_report = hid_get_report,
+	.set_idle = hid_set_idle,
+};
+
+static void
+hid_send_data_cb(void *buf, ssize_t len)
+{
+	hid_send_data(&hid_ctx, &mouse_data, sizeof(struct mouse_data_t), hid_send_data_cb);
+}
+
+static void
+init_my_hid(int config) // see desc.h
+{
+	hid_init(&hid_funcs, &hid_ctx);
+	hid_send_data(&hid_ctx, &mouse_data, sizeof(struct mouse_data_t), hid_send_data_cb);
 }
 
 void
--- examples/hid/vhid.c
+++ examples/hid/vhid.c
@@ -8,24 +8,47 @@ static void init_my_hid(int config);
 
 static struct hid_ctx hid_ctx;
 
-static int
-report_in(uint8_t report_id, void *data_out, size_t data_size)
+static size_t
+hid_get_descriptor(enum hid_report_descriptor_type type, uint8_t index, void **data_out)
 {
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ REPORT IN CALLED");
+	if (type != USB_HID_REPORT_DESC_TYPE_REPORT)
+		return (0);
+	*data_out = report_desc; // see desc.h
+	return (REPORT_DESC_SIZE);
 }
 
-static int
-report_out(uint8_t report_id, void *data_out, size_t data_size)
+static size_t
+hid_get_report(enum hid_report_type type, uint8_t report_id, void **data_out)
 {
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ REPORT OUT CALLED");
+	if (type != USB_HID_REPORT_TYPE_INPUT)
+		return (0);
+	*data_out = &mouse_data;
+	return (sizeof(struct mouse_data_t));
 }
 
 static void
-init_my_hid(int config)
+hid_set_idle(const uint8_t duration, const uint8_t report_id)
 {
-	hid_descriptor.report_input = report_in;
-	hid_descriptor.report_output = report_out;
-	hid_init(&hid_descriptor, &hid_ctx);
+	printf("@@@@@ IDLE SET TO %d\n", duration * 4);
+}
+
+static struct hid_user_functions_t hid_funcs = {
+	.get_descriptor = hid_get_descriptor,
+	.get_report = hid_get_report,
+	.set_idle = hid_set_idle
+};
+
+static void
+hid_send_data_cb(void *buf, ssize_t len)
+{
+	hid_send_data(&hid_ctx, &mouse_data, sizeof(struct mouse_data_t), hid_send_data_cb);
+}
+
+static void
+init_my_hid(int config) // see desc.h
+{
+	hid_init(&hid_funcs, &hid_ctx);
+	hid_send_data(&hid_ctx, &mouse_data, sizeof(struct mouse_data_t), hid_send_data_cb);
 }
 
 int
