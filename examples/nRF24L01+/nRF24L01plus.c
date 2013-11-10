22b104b1cb2227e2e7b11811d88a71dcfeab6b80
Daniel Tralamazza <tralamazza@gmail.com>
2013-11-10 10:01:39 +0100

    please make it stop

--- examples/nRF24L01+/nRF24L01plus.c
+++ examples/nRF24L01+/nRF24L01plus.c
@@ -14,7 +14,8 @@ enum {
 void
 read_register(enum NRF_REG_ADDR reg, void *data, uint16_t len, spi_cb *cb)
 {
-	uint8_t cmd = NRF_CMD_R_REGISTER | (NRF_REG_MASK & reg);
+	static uint8_t cmd = NRF_CMD_R_REGISTER;
+	cmd |= (NRF_REG_MASK & reg);
 	static struct spi_ctx r_register_ctx;
 	spi_queue_xfer(&r_register_ctx, NRF_SPI_CS,
 		&cmd, 1,
@@ -25,7 +26,8 @@ read_register(enum NRF_REG_ADDR reg, void *data, uint16_t len, spi_cb *cb)
 void
 write_register(enum NRF_REG_ADDR reg, uint8_t *data, uint16_t len, spi_cb *cb)
 {
-	uint8_t cmd = NRF_CMD_W_REGISTER | (NRF_REG_MASK & reg);
+	static uint8_t cmd = NRF_CMD_W_REGISTER;
+	cmd |= (NRF_REG_MASK & reg);
 	static struct spi_ctx_bare w_register_ctx;
 	static struct sg tx_sg[2];
 	static struct sg rx_sg;
@@ -39,8 +41,8 @@ write_register(enum NRF_REG_ADDR reg, uint8_t *data, uint16_t len, spi_cb *cb)
 void
 read_rx_payload(uint8_t *data, uint16_t len, spi_cb *cb)
 {
+	static uint8_t cmd = NRF_CMD_R_RX_PAYLOAD;
 	static struct spi_ctx r_rx_payload_ctx;
-	uint8_t cmd = NRF_CMD_R_RX_PAYLOAD;
 	spi_queue_xfer(&r_rx_payload_ctx, NRF_SPI_CS,
 		&cmd, 1,
 		data, len,
@@ -50,7 +52,7 @@ read_rx_payload(uint8_t *data, uint16_t len, spi_cb *cb)
 void
 write_tx_payload(uint8_t *data, uint16_t len, spi_cb *cb)
 {
-	uint8_t cmd = NRF_CMD_W_TX_PAYLOAD;
+	static uint8_t cmd = NRF_CMD_W_TX_PAYLOAD;
 	static struct spi_ctx_bare w_tx_payload_ctx;
 	static struct sg tx_sg[2];
 	spi_queue_xfer_sg(&w_tx_payload_ctx, NRF_SPI_CS,
@@ -62,7 +64,7 @@ write_tx_payload(uint8_t *data, uint16_t len, spi_cb *cb)
 void
 flush_tx(spi_cb *cb)
 {
-	uint8_t cmd = NRF_CMD_FLUSH_TX;
+	static uint8_t cmd = NRF_CMD_FLUSH_TX;
 	static struct spi_ctx flush_tx_ctx;
 	spi_queue_xfer(&flush_tx_ctx, NRF_SPI_CS,
 		&cmd, 1,
@@ -73,7 +75,7 @@ flush_tx(spi_cb *cb)
 void
 flush_rx(spi_cb *cb)
 {
-	uint8_t cmd = NRF_CMD_FLUSH_RX;
+	static uint8_t cmd = NRF_CMD_FLUSH_RX;
 	static struct spi_ctx flush_rx_ctx;
 	spi_queue_xfer(&flush_rx_ctx, NRF_SPI_CS,
 		&cmd, 1,
@@ -84,7 +86,7 @@ flush_rx(spi_cb *cb)
 void
 reuse_tx_pl(spi_cb *cb)
 {
-	uint8_t cmd = NRF_CMD_REUSE_TX_PL;
+	static uint8_t cmd = NRF_CMD_REUSE_TX_PL;
 	static struct spi_ctx reuse_tx_ctx;
 	spi_queue_xfer(&reuse_tx_ctx, NRF_SPI_CS,
 		&cmd, 1,
@@ -101,66 +103,61 @@ void
 read_rx_payload_width_cb(void *data)
 {
 	struct read_rx_payload_width_t *ctx = data;
-	ctx->cb(&ctx->rxbuf[1]); // flush RX if packet width > 32
+	if (ctx->rxbuf[1] > 32) {
+		flush_rx(NULL); // flush RX if packet width > 32
+	}
+	ctx->cb(&ctx->rxbuf[1]);
 }
 
 void
 read_rx_payload_width(spi_cb *cb)
 {
+	static uint8_t cmd = NRF_CMD_R_RX_PL_WID;
 	static struct spi_ctx read_rx_payload_ctx;
 	static struct read_rx_payload_width_t ctx;
-	static uint8_t cmd = NRF_CMD_R_RX_PL_WID;
 	ctx.cb = cb;
 	spi_queue_xfer(&read_rx_payload_ctx, NRF_SPI_CS,
 		&cmd, 1,
 		ctx.rxbuf, 2,
 		read_rx_payload_width_cb, &ctx);
 }
-/*
+
 void
-write_ack_payload(uint8_t *value, uint16_t len)
+write_ack_payload(uint8_t pipe, uint8_t *data, uint16_t len, spi_cb *cb)
 {
-	struct spi_ctx w_ack_payload_ctx;
-	uint8_t cmd = NRF_CMD_W_ACK_PAYLOAD;
-	spi_queue_xfer(&w_ack_payload_ctx, NRF_SPI_CS,
-		&cmd, 1,
-		NULL, 0,
-		NULL, NULL);
-	spi_queue_xfer(&w_ack_payload_ctx, NRF_SPI_CS,
-		value, len,
-		NULL, 0,
-		NULL, NULL);
+	static uint8_t cmd = NRF_CMD_W_ACK_PAYLOAD;
+	cmd |= (pipe & 0x7);
+	static struct spi_ctx_bare w_ack_payload_ctx;
+	static struct sg tx_sg[2];
+	spi_queue_xfer_sg(&w_ack_payload_ctx, NRF_SPI_CS,
+		sg_init(tx_sg, &cmd, 1, data, len),
+		NULL,
+		cb, NULL);
 }
 
 void
-write_tx_payload_no_ack()
+write_tx_payload_no_ack(uint8_t *data, uint16_t len, spi_cb *cb)
 {
-	static struct spi_ctx w_tx_payload_ctx;
-	uint8_t cmd = NRF_CMD_W_TX_PAYLOAD;
-	spi_queue_xfer(&w_tx_payload_ctx, NRF_SPI_CS,
-		&cmd, 1,
-		NULL, 0,
-		NULL, NULL);
-	spi_queue_xfer(&w_tx_payload_ctx, NRF_SPI_CS,
-		value, len,
-		NULL, 0,
-		NULL, NULL);
-}*/
+	static uint8_t cmd = NRF_CMD_W_TX_PAYLOAD_NO_ACK;
+	static struct spi_ctx_bare w_tx_payload_ctx;
+	static struct sg tx_sg[2];
+	spi_queue_xfer_sg(&w_tx_payload_ctx, NRF_SPI_CS,
+		sg_init(tx_sg, &cmd, 1, data, len),
+		NULL,
+		cb, NULL);
+}
 
 void
-nop(void)
+nop(spi_cb *cb)
 {
-	static struct spi_ctx nop_ctx;
 	static uint8_t cmd = NRF_CMD_NOP;
+	static struct spi_ctx nop_ctx;
+	static uint8_t rx_buff;
 	spi_queue_xfer(&nop_ctx, NRF_SPI_CS,
 		&cmd, 1,
-		NULL, 0,
-		NULL, NULL);
+		&rx_buff, 1,
+		cb, &rx_buff);
 }
-
-struct nrf_disable_shockburst_t {
-
-};
 
 void
 PORTC_Handler(void)
