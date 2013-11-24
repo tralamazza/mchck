34b31d7608350ab3808247884ad946e0f4dc4e29
Daniel Tralamazza <tralamazza@gmail.com>
2013-11-24 13:36:15 +0100

    calc temp in deg

--- examples/nRF24L01+/test_btle.c
+++ examples/nRF24L01+/test_btle.c
@@ -2,16 +2,18 @@
 #include "nRF24L01plus.h"
 
 static struct timeout_ctx t;
-static volatile int temp_cnt = -1; // give us some time
 #define READ_TEMP_PERIOD 500
 
 static void
 temp_done(uint16_t data, int error, void *cbdata)
 {
-	static unsigned accum volt;
+	static accum temp_deg;
 	onboard_led(ONBOARD_LED_TOGGLE);
-	volt = adc_as_voltage(data);
-	nrf_btle_advertise(&volt, sizeof(unsigned accum));
+	unsigned accum volt = adc_as_voltage(data);
+	accum volt_diff = volt - 0.719k;
+	accum temp_diff = volt_diff * (1000K / 1.715K);
+    temp_deg = 25k - temp_diff;
+	nrf_btle_advertise(&temp_deg, sizeof(accum));
 }
 
 static void
