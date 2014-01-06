ae30230504f743c1084fcc97987eddb81184c08e
Daniel Tralamazza <tralamazza@gmail.com>
2014-01-06 03:25:20 +0100

    [dma] add a src/dst addr adjusment and dma cancel

--- toolchain/lib/mchck/dma.c
+++ toolchain/lib/mchck/dma.c
@@ -1,7 +1,7 @@
 #include <mchck.h>
 
 static struct dma_ctx_t {
-	dma_callback* cb;
+	dma_cb* cb;
 } ctx[4];
 
 void
@@ -39,27 +39,29 @@ dma_set_arbitration(enum dma_arbitration_t arb)
 }
 
 void
-dma_from(uint8_t ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod)
+dma_from(enum dma_channel ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod)
 {
 	volatile struct DMA_TCD_t* tcd = &DMA.tcd[ch];
 	tcd->saddr = (uint32_t)addr;
 	tcd->soff = off;
 	tcd->attr.ssize = tsize;
 	tcd->attr.smod = mod;
+	tcd->slast = -(1 << tsize); // adjust to tsize by default
 }
 
 void
-dma_to(uint8_t ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod)
+dma_to(enum dma_channel ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod)
 {
 	volatile struct DMA_TCD_t* tcd = &DMA.tcd[ch];
 	tcd->daddr = (uint32_t)addr;
 	tcd->doff = off;
 	tcd->attr.dsize = tsize;
 	tcd->attr.dmod = mod;
+	tcd->dlastsga = -(1 << tsize); // adjust to tsize by default
 }
 
 void
-dma_major_loop_count(uint8_t ch, uint16_t iter)
+dma_major_loop_count(enum dma_channel ch, uint16_t iter)
 {
 	volatile struct DMA_TCD_t* tcd = &DMA.tcd[ch];
 	if (tcd->citer.elinkyes.elink) {
@@ -72,7 +74,7 @@ dma_major_loop_count(uint8_t ch, uint16_t iter)
 }
 
 void
-dma_start(uint8_t ch, enum dma_mux_source_t source, uint8_t tri, dma_callback* cb)
+dma_start(enum dma_channel ch, enum dma_mux_source_t source, uint8_t tri, dma_cb* cb)
 {
 	ctx[ch].cb = cb;
 	volatile struct DMAMUX_t *mux = &DMAMUX0[ch];
@@ -86,27 +88,46 @@ dma_start(uint8_t ch, enum dma_mux_source_t source, uint8_t tri, dma_callback* c
 }
 
 void
-dma_set_priority(uint8_t ch, uint8_t prio)
+dma_cancel(enum dma_channel ch)
+{
+	if (!DMA.tcd[ch].csr.active)
+		DMA.cr.cx = 1; // .cr cannot be changed on active channels
+}
+
+void
+dma_set_priority(enum dma_channel ch, uint8_t prio)
 {
 	DMA.dchpri[ch].chpri = prio;
 }
 
 void
-dma_enable_channel_preemption(uint8_t ch, uint8_t on)
+dma_enable_channel_preemption(enum dma_channel ch, uint8_t on)
 {
 	DMA.dchpri[ch].ecp = on;
 }
 
 void
-dma_enable_preempt_ability(uint8_t ch, uint8_t on)
+dma_enable_preempt_ability(enum dma_channel ch, uint8_t on)
 {
 	DMA.dchpri[ch].dpa = !on;
 }
 
 void
+dma_from_addr_adj(enum dma_channel ch, uint32_t off)
+{
+	DMA.tcd[ch].slast = off;
+}
+
+void
+dma_to_addr_adj(enum dma_channel ch, uint32_t off)
+{
+	DMA.tcd[ch].dlastsga = off;
+}
+
+void
 DMA0_Handler(void)
 {
-	uint8_t ch = *(uint8_t*)&DMA._int;
+	enum dma_channel ch = *(uint8_t*)&DMA._int;
 	uint8_t curr = DMA.tcd[ch].citer.elinkyes.elink ? DMA.tcd[ch].citer.elinkyes.citer : DMA.tcd[ch].citer.elinkno.citer;
 	ctx[ch].cb(ch, 0, curr);
 	DMA.cint.cint = ch; // clear
@@ -139,5 +160,5 @@ DMA_error_Handler(void)
 	if (DMA.es.ecx)
 		err |= DMA_ERR_TRNFS_CANCL;
 	ctx[DMA.es.errchn].cb(DMA.es.errchn, err, 0);
-	DMA.cerr.cerr = DMA.es.errchn;
+	DMA.cerr.cerr = DMA.es.errchn; // clear
 }
--- toolchain/lib/mchck/dma.h
+++ toolchain/lib/mchck/dma.h
@@ -3,6 +3,13 @@ enum dma_arbitration_t {
 	DMA_ARBITRATION_ROUND_ROBIN    = 0x1
 };
 
+enum dma_channel {
+	DMA_CH_0 = 0x0,
+	DMA_CH_1 = 0x1,
+	DMA_CH_2 = 0x2,
+	DMA_CH_3 = 0x3
+};
+
 #define DMA_ERR_DST_BUS      0x1
 #define DMA_ERR_SRC_BUS      0x2
 #define DMA_ERR_SG_CONF      0x4
@@ -14,14 +21,17 @@ enum dma_arbitration_t {
 #define DMA_ERR_CHNL_PRIO    0x100
 #define DMA_ERR_TRNFS_CANCL  0x200
 
-typedef void (dma_callback)(uint8_t ch, uint32_t err, uint8_t major);
+typedef void (dma_cb)(enum dma_channel ch, uint32_t err, uint8_t major);
 
 void dma_init(void);
 void dma_set_arbitration(enum dma_arbitration_t arb);
-void dma_from(uint8_t ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod);
-void dma_to(uint8_t ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod);
-void dma_major_loop_count(uint8_t ch, uint16_t iter);
-void dma_start(uint8_t ch, enum dma_mux_source_t source, uint8_t tri, dma_callback* cb);
-void dma_set_priority(uint8_t ch, uint8_t prio);
-void dma_enable_channel_preemption(uint8_t ch, uint8_t on);
-void dma_enable_preempt_ability(uint8_t ch, uint8_t on);
+void dma_from(enum dma_channel ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod);
+void dma_to(enum dma_channel ch, void* addr, size_t count, enum dma_transfer_size_t tsize, size_t off, uint8_t mod);
+void dma_major_loop_count(enum dma_channel ch, uint16_t iter);
+void dma_start(enum dma_channel ch, enum dma_mux_source_t source, uint8_t tri, dma_cb* cb);
+void dma_cancel(enum dma_channel ch);
+void dma_set_priority(enum dma_channel ch, uint8_t prio);
+void dma_enable_channel_preemption(enum dma_channel ch, uint8_t on);
+void dma_enable_preempt_ability(enum dma_channel ch, uint8_t on);
+void dma_from_addr_adj(enum dma_channel ch, uint32_t off);
+void dma_to_addr_adj(enum dma_channel ch, uint32_t off);
