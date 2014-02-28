59e69434fe6ad2fc216dd002aafe97b5c9d5c0c3
Daniel Tralamazza <tralamazza@gmail.com>
2014-02-28 15:32:32 +0100

    nrf: change defaults to match RF24 ping example

--- examples/nrf/nrf.c
+++ examples/nrf/nrf.c
@@ -5,17 +5,14 @@
 
 #define RX_SIZE 32
 
+#define RX_ADDR 0xf0f0f0f0e1ll
+#define TX_ADDR 0xf0f0f0f0d2ll
+
 static int32_t tx_buffer = 0;
 static uint8_t rx_buffer[RX_SIZE];
 
-static struct nrf_addr_t target_addr = {
-	.value =  0xf0f0f0f0d2ll,
-	.size = 5
-};
-static struct nrf_addr_t my_addr = {
-	.value =  0xf0f0f0f0e1ll,
-	.size = 5
-};
+static struct nrf_addr_t target_addr = { .value =  TX_ADDR, .size = 5 };
+static struct nrf_addr_t my_addr = { .value =  RX_ADDR, .size = 5 };
 
 static void wait_for_ping();
 static void send_ping();
@@ -23,8 +20,6 @@ static void send_ping();
 static void
 ping_sent(void *data, uint8_t len)
 {
-	if (!data)
-		onboard_led(ONBOARD_LED_TOGGLE); // send failed, max RT reached
 	wait_for_ping();
 }
 
@@ -37,6 +32,7 @@ send_ping()
 static void
 ping_received(void *data, uint8_t len)
 {
+	onboard_led(ONBOARD_LED_TOGGLE);
 	tx_buffer = *(int32_t*)data;
 	send_ping();
 }
@@ -52,12 +48,11 @@ main(void)
 {
 	nrf_init();
 	nrf_set_autoretransmit(3, 5); // 1000us, 5x
-	nrf_set_channel(16);
+	nrf_set_crc_length(NRF_CRC_ENC_2_BYTES); // CRC16
+	nrf_set_channel(76);
 	nrf_set_power_datarate(NRF_TX_POWER_0DBM, NRF_DATA_RATE_1MBPS);
 	nrf_enable_dynamic_payload();
 	// nrf_enable_powersave();
 	send_ping();
 	sys_yield_for_frogs();
 }
-
-/* vim: set ts=8 sw=8 noexpandtab: */
