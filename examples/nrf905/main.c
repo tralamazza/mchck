0d975f3402605d87c73500b922c4051d5b753fd8
Daniel Tralamazza <tralamazza@relayr.de>
2014-09-29 23:58:27 +0200

    cdc nrf bridge

--- examples/nrf905/main.c
+++ examples/nrf905/main.c
@@ -3,13 +3,13 @@
 #include "nrf905_usb.desc.h"
 
 #define ADDR_LEN 4
+#define PAYLOAD_LEN 32
 
 static struct cdc_ctx cdc;
 static struct nrf905_ctx_t ctx;
 static uint8_t my_addr[ADDR_LEN] = { 0xaa, 0xaa, 0xaa, 0xaa };
 static uint8_t target_addr[ADDR_LEN] = { 0xbb, 0xbb, 0xbb, 0xbb };
-static uint8_t payload[32];
-static uint8_t payload_len = 32;
+static uint8_t payload[PAYLOAD_LEN];
 
 static enum {
 	APP_STATE_INIT,
@@ -22,6 +22,13 @@ nrf905_recv_done(void *data, uint8_t len)
 {
 	onboard_led(ONBOARD_LED_TOGGLE);
 	cdc_write(data, len, &cdc);
+}
+
+static void
+nrf905_send_done(void *data, uint8_t len)
+{
+	cdc_read_more(&cdc);
+	nrf905_receive(&ctx, payload, PAYLOAD_LEN, nrf905_recv_done);
 }
 
 static void
@@ -38,7 +45,7 @@ nrf905_app_state_handler(void *data)
 		app_state = APP_STATE_DONE;
 		nrf905_set_tx_addr(&ctx, target_addr, ADDR_LEN, nrf905_app_state_handler);
 	case APP_STATE_DONE:
-		nrf905_receive(&ctx, payload, payload_len, nrf905_recv_done);
+		nrf905_receive(&ctx, payload, PAYLOAD_LEN, nrf905_recv_done);
 		break;
 	}
 }
@@ -46,14 +53,19 @@ nrf905_app_state_handler(void *data)
 static void
 cdc_data_sent(size_t len)
 {
-	nrf905_receive(&ctx, payload, payload_len, nrf905_recv_done);
+}
+
+static void
+cdc_new_data(uint8_t *data, size_t len)
+{
+	nrf905_send(&ctx, data, len, nrf905_send_done);
 }
 
 void
 init_vcdc(int enable)
 {
 	if (enable) {
-		cdc_init(NULL, cdc_data_sent, &cdc);
+		cdc_init(cdc_new_data, cdc_data_sent, &cdc);
 	}
 }
 
